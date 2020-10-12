3297bba97cdb2a53c76537c2396d99e61267fe14
Emmanuel <emanuel2202zch@gmail.com>
2020-10-11 20:43:18 -0600

    Agregado Modelo de interfaz y archivo c

--- proyecto/View/pthreadmon_ui.c
+++ proyecto/View/pthreadmon_ui.c
@@ -1,55 +1,43 @@
 #include <gtk/gtk.h>
-#include <stdlib.h>
-//#include "../Model/mapper.h"
 
-
-static void inicial_message(GtkWidget * widget, gpointer data) {
-  g_print("Pokemon Go Battle Simulator\n");
+static void print_message (GtkWidget *widget, gpointer data) {
+    g_print ("Pokemon Go Battle Simulator\n");
 }
 
+static void activate (GtkApplication *app, gpointer user_data)
+{
+    GtkWidget *window;
+    GtkWidget *button;
+    GtkWidget *button_box;
+    GtkWidget * image;
 
-static void my_callback(GObject *source_object, GAsyncResult *res, gpointer user_data) {}
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Pthreadmon");
+    gtk_window_set_default_size (GTK_WINDOW (window), 500, 400);
 
-/* static void start_async(GTask *task, gpointer source_object, gpointer task_data, GCancellable *cancellable) {
-    start_race();
-}
-  */
-static void start_clicked(){
-    GCancellable *cancellable = g_cancellable_new();
-    GTask *task = g_task_new(g_object_new(G_TYPE_OBJECT, NULL), cancellable, my_callback, NULL);
-    g_task_run_in_thread(task, start_async);
-    g_object_unref(task);
-}
+    button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+    gtk_container_add (GTK_CONTAINER (window), button_box);
 
-static void activate(GtkApplication* app, gpointer user_data){
-    GtkWidget * window;
-    GtkWidget * button;
-    GtkWidget * button_box;
+    image = gtk_image_new_from_file ("/Images/Pokemon_logo.png");
+    gtk_container_add(GTK_CONTAINER (window), image);
 
-  window = gtk_application_window_new(app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 300,500);
+    button = gtk_button_new_with_label ("Start!");
+    g_signal_connect (button, "clicked", G_CALLBACK (print_message), NULL);
+    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+    gtk_container_add (GTK_CONTAINER (button_box), button);
 
-  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER (window), button_box);
-
-  button = gtk_button_new_with_label("Pokemon Go Simulator");
-  g_signal_connect(button, "clicked", G_CALLBACK (inicial_message), NULL);
-  g_signal_connect_swapped(button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-  gtk_container_add (GTK_CONTAINER (button_box), button);
-
-  gtk_widget_show_all(window);
+    gtk_widget_show_all (window);
 }
 
-
-int main(int argc, char * argv[]){
-    GtkApplication * app;
+int
+main (int argc, char **argv) {
+    GtkApplication *app;
     int status;
 
-    app = gtk_application_new("PthreadMon - Pokemon Go Simulator", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+    app = gtk_application_new ("pthreadmon.simulator.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
 
     return status;
 }
