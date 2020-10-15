e123d3f35e6792f322d542db5012fc1f437e1299
Gabriel Umana <gabrieluf2297@gmail.com>
2020-10-14 23:38:45 -0600

    Estoy pensando en como hacer para despertar al hilo del siguiente jugador para que ataque, y asi sucesivamente...

--- proyecto/Controller/BattleController/BattleController.c
+++ proyecto/Controller/BattleController/BattleController.c
@@ -13,6 +13,7 @@ typedef struct
     clock_t end_time;
     pthread_mutex_t mutex;
     pthread_barrier_t barrier;
+    pthread_cond_t condition;
 } battle_data_t;
 
 typedef struct
@@ -20,14 +21,12 @@ typedef struct
     size_t thread_num;
     battle_data_t *battle_data;
     pokemon_t *pokemon;
-    player_t* player;
+    player_t *player;
     double fighting_time;
 } player_data_t;
 
-
 void *fight(void *args)
 {
-
     // obtener los datos que nos enviaron al crear el hilo
     player_data_t *local_data = (player_data_t *)args;
     battle_data_t *shared_data = local_data->battle_data;
@@ -44,9 +43,22 @@ void *fight(void *args)
     // ahora esperar en la barrera a que lleguen todos los hilos para asi empezar la batalla
     printf("thread[%zu] - jugador %s esperando en la barrera...\n", local_data->thread_num, local_data->player->nickname);
     pthread_barrier_wait(&shared_data->barrier);
-    printf("thread[%zu] - jugador %s uso %s\n", local_data->thread_num, local_data->player->nickname,local_data->pokemon->pokemon_info->speciesName);
+    printf("thread[%zu] - jugador %s uso %s\n", local_data->thread_num, local_data->player->nickname, local_data->pokemon->pokemon_info->speciesName);
 
-    //TODO: listo, podemos empezar a atacar
+    // Mientras la vida del pokemon sea mayor a 0
+    // while (local_data->pokemon->hp > 0)
+    // {
+    //     pthread_mutex_lock(&shared_data->mutex);
+    //     // voy a revisar si es mi turno. Los pokemones se crean de manera secuencial, a como se ingresaron en el arreglo
+    //     // (como los especifico el usuario), entonces el numero de hilo debe ser igual al numero del pokemon que esta compitieno.
+    //     if (local_data->thread_num == shared_data->player1_turn)
+    //     {
+    //         // no es mi turno, dormir
+    //         pthread_cond_wait(&shared_data->condition, &shared_data->mutex);
+    //     }
+    // }
+    // // pokemon murio, aumentar el contador y soltar el mutex
+    // pthread_mutex_unlock(&shared_data->mutex);
 
     return NULL;
 }
@@ -59,13 +71,14 @@ void initBattle(player_t *player1, player_t *player2)
 
     // crear los datos compartidos por los hilos
     battle_data_t *shared_battle_data = (battle_data_t *)malloc((MAX_POKEMONS_PER_PLAYER * 2) * sizeof(battle_data_t));
-    shared_battle_data->player1_turn = 1;
-    shared_battle_data->player2_turn = 1;
+    shared_battle_data->player1_turn = 0;
+    shared_battle_data->player2_turn = 0;
     shared_battle_data->isStartTimeSet = 0;
 
     // inicializar los metodos de sincronizacion
     pthread_mutex_init(&shared_battle_data->mutex, NULL);
     pthread_barrier_init(&shared_battle_data->barrier, NULL, (MAX_POKEMONS_PER_PLAYER * 2));
+    pthread_cond_init(&shared_battle_data->condition, NULL);
 
     // crear los datos necesarios por hilo
     player_data_t *player1_data = (player_data_t *)malloc(MAX_POKEMONS_PER_PLAYER * sizeof(player_data_t));
@@ -105,6 +118,8 @@ void initBattle(player_t *player1, player_t *player2)
 
     // liberar datos
     pthread_mutex_destroy(&shared_battle_data->mutex);
+    pthread_barrier_destroy(&shared_battle_data->barrier);
+    pthread_cond_destroy(&shared_battle_data->condition);
     free(player1_data);
     free(player2_data);
     free(shared_battle_data);
